6f8c04fabb02c6b012659ca44181d29e9eec40be
rainrambler <wanganyu@outlook.com>
2015-10-26 22:43:51 +0800

    Added some parser methods

--- demangler.go
+++ demangler.go
@@ -15,6 +15,8 @@ type Demangler struct {
 	CVqualifiers []byte
 	RefQualifer  byte
 	FuncName     string
+	isCtor       bool
+	isDtor       bool
 }
 
 func isNestedName(mangled string) bool {
@@ -42,6 +44,10 @@ func isRefqualifier(mangled string) bool {
 	// the first char
 	return (mangled[0] == 'R') ||
 		(mangled[0] == 'O')
+}
+
+func isNumberChar(c byte) bool {
+	return (c >= '0') && (c <= '9')
 }
 
 func (p *Demangler) unmangle(mangled string) {
@@ -80,12 +86,21 @@ func (p *Demangler) parsePrefix() {
 }
 
 func (p *Demangler) parseUnqualifiedName() {
+	var res bool
+	res = p.parseCtorDtorName()
 	
+	if !res {
+		res = p.parseOperatorName()
+	}
+	
+	if !res {
+		res = p.parseUnnamedTypeName()
+	}
 }
 
-func (p *Demangler) parseOperatorName() {
-	if len(p.Remain < 2) {
-		return
+func (p *Demangler) parseOperatorName() bool {
+	if len(p.Remain) < 2 {
+		return false
 	}
 	
 	op := p.Remain[0:2]
@@ -94,62 +109,181 @@ func (p *Demangler) parseOperatorName() {
 	} else if (op == "na") {
 		p.FuncName = "new[]"
 	} else if (op == "dl") {
-		
+		p.FuncName = "delete"
 	} else if (op == "da") {
-		
+		p.FuncName = "delete[]"
 	} else if (op == "ps") {
-		
+		p.FuncName = "+ (unary)"
 	} else if (op == "ng") {
-		
+		p.FuncName = "- (unary)"
 	} else if (op == "ad") {
-		
+		p.FuncName = "& (unary)"
 	} else if (op == "de") {
-		
+		p.FuncName = "* (unary)"
 	} else if (op == "co") {
-		
+		p.FuncName = "~"
 	} else if (op == "pl") {
-		
+		p.FuncName = "+"
 	} else if (op == "mi") {
-		
+		p.FuncName = "-"
 	} else if (op == "ml") {
-		
+		p.FuncName = "*"
 	} else if (op == "dv") {
-		
+		p.FuncName = "/"
 	} else if (op == "rm") {
-		
+		p.FuncName = "%"
 	} else if (op == "an") {
-		
+		p.FuncName = "&"
 	} else if (op == "or") {
-		
+		p.FuncName = "|"
 	} else if (op == "eo") {
-		
+		p.FuncName = "^"
 	} else if (op == "aS") {
-		
+		p.FuncName = "="
 	} else if (op == "pL") {
-		
+		p.FuncName = "+="
 	} else if (op == "mI") {
-		
+		p.FuncName = "-="
 	} else if (op == "mL") {
-		
+		p.FuncName = "*="
 	} else if (op == "dV") {
-		
+		p.FuncName = "/="
 	} else if (op == "rM") {
-		
+		p.FuncName = "%="
 	} else if (op == "aN") {
-		
+		p.FuncName = "&="
 	} else if (op == "oR") {
-		
+		p.FuncName = "|="
 	} else if (op == "eO") {
-		
-	} 
+		p.FuncName = "^="
+	} else if (op == "ls") {
+		p.FuncName = "<<"
+	} else if (op == "rs") {
+		p.FuncName = ">>"
+	} else if (op == "lS") {
+		p.FuncName = "<<="
+	} else if (op == "rS") {
+		p.FuncName = ">>="
+	} else if (op == "eq") {
+		p.FuncName = "=="
+	} else if (op == "ne") {
+		p.FuncName = "!="
+	} else if (op == "lt") {
+		p.FuncName = "<"
+	} else if (op == "gt") {
+		p.FuncName = ">"
+	} else if (op == "le") {
+		p.FuncName = "<="
+	} else if (op == "ge") {
+		p.FuncName = ">="
+	} else if (op == "nt") {
+		p.FuncName = "!"
+	} else if (op == "aa") {
+		p.FuncName = "&&"
+	} else if (op == "oo") {
+		p.FuncName = "||"
+	} else if (op == "pp") {
+		p.FuncName = "++"
+	} else if (op == "mm") {
+		p.FuncName = "--"
+	} else if (op == "cm") {
+		p.FuncName = ","
+	} else if (op == "pm") {
+		p.FuncName = "->*"
+	} else if (op == "pt") {
+		p.FuncName = "->"
+	} else if (op == "cl") {
+		p.FuncName = "()"
+	} else if (op == "ix") {
+		p.FuncName = "[]"
+	} else if (op == "qu") {
+		p.FuncName = "?"
+	} else if (op == "cv") {
+		p.FuncName = "(cast)" // ?
+	} else if (op == "li") {
+		p.FuncName = ""
+	} else if (op[0] == 'v') {
+		p.FuncName = "" // ?
+	} else {
+		p.FuncName = ""
+		return false
+	}
+	
+	p.Remain = p.Remain[2:]
+	return true
+}
+
+func (p *Demangler) parseCtorDtorName() bool {
+	if len(p.Remain) < 2 {
+		return false
+	}
+	
+	nm := p.Remain[0:2]
+	if nm == "C1" {
+		p.isCtor = true
+	} else if nm == "C2" {
+		p.isCtor = true
+	} else if nm == "C3" {
+		p.isCtor = true
+	} else if nm == "D0" {
+		p.isDtor = true
+	} else if nm == "D1" {
+		p.isDtor = true
+	} else if nm == "D2" {
+		p.isDtor = true
+	} else {
+		return false
+	}
+	
+	p.Remain = p.Remain[2:]
+	return true
+}
+
+func (p *Demangler) parseUnnamedTypeName() bool {
+	if len(p.Remain) < 2 {
+		return false
+	}
+	
+	nm := p.Remain[0:2]
+	if nm == "Ut" {
+		// TODO
+		fmt.Printf("WARN: parseUnnamedTypeName not implemented! Value: %v\n", p.Remain)
+	} else {
+		return false
+	}
+	
+	p.Remain = p.Remain[2:]
+	return true
 }
 
 func (p *Demangler) parseTemplatePrefix() {
 	
 }
 
-func (p *Demangler) parseSourceName() {
+func (p *Demangler) parseSourceName() string {
+	if len(p.Remain) < 2 {
+		return ""
+	}
 	
+	c0 := p.Remain[0]
+	c1 := p.Remain[1]
+	charLen := 0
+	if isNumberChar(c0) && isNumberChar(c1) {
+		charLen = (int(c0 - '0') * 10) + int(c1 - '0')
+		p.Remain = p.Remain[2:]
+		
+		s := p.Remain[0:charLen]
+		return s
+	} else if isNumberChar(c0) {
+		charLen = int(c0 - '0')
+		p.Remain = p.Remain[1:]
+		s := p.Remain[0:charLen]
+		return s
+	} else {
+		// some error
+		fmt.Printf("WARN: Invalid source name: %v\n", p.Remain)
+		return ""
+	}
 }
 
 func (p *Demangler) parseIdentifier() {
