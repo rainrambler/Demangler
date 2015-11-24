742f2180263614dbed2fbeeaa412be918cc128bb
rainrambler <wanganyu@outlook.com>
2015-11-24 22:15:06 +0800

    added isUpperChar

--- demangler.go
+++ demangler.go
@@ -109,6 +109,10 @@ func isNumberChar(c byte) bool {
 	return (c >= '0') && (c <= '9')
 }
 
+func isUpperChar(c byte) bool {
+	return (c >= 'A') && (c <= 'Z')
+}
+
 func isNonZeroNumberChar(c byte) bool {
 	return (c >= '1') && (c <= '9')
 }
